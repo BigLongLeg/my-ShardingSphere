820a87e5f1cf474a07ac9d88c91711292dc60b30
zhangzhiqiang <zhangzhiqiang@aibanshou.com>
2020-10-26 23:05:23 +0800

    根据标准策略实现分库分表

--- src/test/java/com/example/demo/MyShardingSphereTests.java
+++ src/test/java/com/example/demo/MyShardingSphereTests.java
@@ -7,13 +7,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.ActiveProfiles;
 
 import javax.annotation.Resource;
 import javax.sql.DataSource;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
-
+@ActiveProfiles("stand")
 @SpringBootTest(classes = MyShardingSphereApplication.class)
 class MyShardingSphereTests {
     @Resource
@@ -34,8 +35,8 @@ class MyShardingSphereTests {
         Random random = new Random();
         for (int i = 0; i < 10000; i++) {
             Long aLong = (Long) keyGenerator.generateKey();
-            jdbcTemplate.update("insert ignore into t_order (user_id,order_id) values (?,?)",
-                    aLong ,random.nextInt(10000));
+            jdbcTemplate.update("insert  into t_order (user_id,order_id) values (?,?)",
+                    aLong ,Integer.valueOf(random.nextInt(10000)).longValue() );
         }
     }
 
